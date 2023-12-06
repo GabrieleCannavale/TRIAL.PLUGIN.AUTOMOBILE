4c26cc57562294d74d2b6775b135106fc7a13221
GabrieleCannavale <gabriele.cannavale92@gmail.com>
2023-12-06 10:55:15 +0100

    COMMIT OK

--- PreCreate.cs
+++ PreCreate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace TRIAL.PLUGIN.AUTOMOBILE
 {
-    public class Class1
+    public class PreCreate :IPlugin
     {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService =
+              (ITracingService)serviceProvider.GetService(typeof(ITracingService)); 
+
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+
+        }
     }
 }
